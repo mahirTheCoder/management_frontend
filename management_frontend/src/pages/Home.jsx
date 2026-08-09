428ddfc8603aa5e980d5e55cc12208f290aaa450
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-08-10 00:15:46 +0600

    ff

---
+++ management_frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container pt-5 text-2xl font-bold text-green-500 '>Home working on frontend</div>
+  )
+}
+
+export default Home
