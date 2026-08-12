e6e4f35661bf92fbac282c174f4500c17a0a8203
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-08-12 14:26:09 +0600

    proteccted route working

--- management_frontend/src/App.jsx
+++ management_frontend/src/App.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
-import { AuthProvider } from "./context/AuthContext";
 import SignIn from "./pages/SignIn";
+
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./Componets/ui/ProtectedRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          {/* Public Route */}
           <Route path="/login" element={<SignIn />} />
+
+          {/* Protected Route */}
         </Routes>
       </AuthProvider>
     </BrowserRouter>
---
+++ management_frontend/src/Componets/ui/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navigate } from "react-router";
+import { useAuth } from "../../context/AuthContext";
+
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  // Profile check এখনো শেষ হয়নি
+  if (loading) return null;
+
+  // Login করা নেই
+  if (!user) return <Navigate to="/login" replace />;
+
+  // Login করা আছে
+  return children;
+};
+
+export default ProtectedRoute
