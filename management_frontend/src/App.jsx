d30a001e0ece74a5c93382ec41cc3a814d1a244e
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-08-14 22:47:46 +0600

    change

--- management_frontend/src/App.jsx
+++ management_frontend/src/App.jsx
@@ -1,18 +1,17 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-
 
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./Componets/ui/ProtectedRoute";
 
+// Auth
 import ForgotPassword from "./pages/Auth/ForgotPassword";
 import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
 import VerifyOTP from "./pages/Auth/VerifyOTP";
 import ResendOTP from "./pages/Auth/ResendOTP";
 import ResetPassword from "./pages/Auth/ResetPassword";
+
+// Admin
 import AdminLayout from "./Componets/Layout/AdminLayout";
 import Admin from "./pages/Admin_Dashboard/Admin";
 
@@ -20,28 +19,47 @@ const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
+
         <Routes>
+
+          {/* ------------- PUBLIC ROUTES  */}
+
+          <Route path="/signIn" element={<SignIn />} />
+
+          <Route path="/signUp" element={<SignUp />} />
+
+          <Route path="/VerifyOTP" element={<VerifyOTP />} />
+
+          <Route path="/ResendOTP" element={<ResendOTP />} />
+
           <Route
-            path="/"
+            path="/ForgotPassword"
+            element={<ForgotPassword />}
+          />
+
+          <Route
+            path="/ResetPassword"
+            element={<ResetPassword />}
+          />
+
+
+          {/* --------- ADMIN ROUTES  */}
+
+          <Route
+            path="/admin"
             element={
-              <ProtectedRoute>
+              <ProtectedRoute allowedRoles={["admin"]}>
                 <AdminLayout />
               </ProtectedRoute>
             }
-          />
-                    <Route index element={<Admin/>} />
+          >
+            {/* /admin */}
+            <Route index element={<Admin />} />
 
-          
-          {/* Public Route */}
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/VerifyOTP" element={<VerifyOTP />} />
-          <Route path="/ResendOTP" element={<ResendOTP/>} />
-          <Route path="/ForgotPassword" element={<ForgotPassword />} />
-          <Route path="/ResetPassword" element={<ResetPassword />} />
+          </Route>
 
-          {/* Protected Route */}
         </Routes>
+
       </AuthProvider>
     </BrowserRouter>
   );
--- management_frontend/src/Componets/ui/ProtectedRoute.jsx
+++ management_frontend/src/Componets/ui/ProtectedRoute.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import { Navigate } from "react-router";
+import React from "react";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
-
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, allowedRoles = [] }) => {
   const { user, loading } = useAuth();
 
-  // Profile check এখনো শেষ হয়নি
-  if (loading) return null;
+  // Profile check এখনো শেষ হয়নি
+  if (loading) {
+    return null;
+  }
 
   // Login করা নেই
-  if (!user) return <Navigate to="/signIn" replace />;
+  if (!user) {
+    return <Navigate to="/signIn" replace />;
+  }
 
-  // Login করা আছে
+  // Role check
+  if (
+    allowedRoles.length > 0 &&
+    !allowedRoles.includes(user.role)
+  ) {
+    return <Navigate to="/signIn" replace />;
+  }
+
+  // Login + authorized role
   return children;
 };
 
-export default ProtectedRoute
+export default ProtectedRoute;
